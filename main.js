f9b9f9e8c826f64355d81bdc807324e0ee2ab094
Artur Talimonchyk <artur.talimonchyk@gmail.com>
2020-08-03 19:54:13 +0300

    firstTry

---
+++ main.js
@@ -0,0 +1,7 @@
+let container = document.querySelector("div");
+let mainImg = container.querySelector(".mainImg");
+let img = mainImg.querySelector("img");
+let leftArrow = mainImg.before;
+let rightArrow = mainImg.after;
+const src = ["img/1.jpg","img/2.jpg","img/3.jpg","img/4.jpg",
+"img/5.jpg","img/6.jpg","img/7.jpg","img/8.jpg"];
